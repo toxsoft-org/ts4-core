dd61fb21e4c5119622a04a542c7b81ca2f0af31f
goga <goga@GOGAT-PC-LIN>
2022-06-28 14:39:53 +0400

    VED

--- org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/glib/library/VedComponentProviderM5Model.java
+++ org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/glib/library/VedComponentProviderM5Model.java
@@ -1,12 +1,16 @@
 package org.toxsoft.core.tsgui.ved.glib.library;
 
 import static org.toxsoft.core.tslib.ITsHardConstants.*;
+import static org.toxsoft.core.tslib.av.metainfo.IAvMetaConstants.*;
 
+import org.eclipse.swt.graphics.*;
+import org.toxsoft.core.tsgui.graphics.icons.*;
 import org.toxsoft.core.tsgui.m5.*;
 import org.toxsoft.core.tsgui.m5.model.*;
 import org.toxsoft.core.tsgui.m5.model.impl.*;
 import org.toxsoft.core.tsgui.m5.std.fields.*;
 import org.toxsoft.core.tsgui.ved.api.library.*;
+import org.toxsoft.core.tslib.av.*;
 import org.toxsoft.core.tslib.coll.*;
 import org.toxsoft.core.tslib.utils.errors.*;
 
@@ -24,19 +28,35 @@ public class VedComponentProviderM5Model
   public static final String MODEL_ID = TS_ID + ".gui.ved.ComponentProvider"; //$NON-NLS-1$
 
   /**
-   * {@link IVedComponentProvider#iconId()}
-   */
-  public static final IM5AttributeFieldDef<IVedComponentProvider> ICON = new M5StdFieldDefParamIconId<>();
-
-  /**
    * {@link IVedComponentProvider#id()}
    */
-  public static final IM5AttributeFieldDef<IVedComponentProvider> ID = new M5StdFieldDefId<>();
+  public static final IM5AttributeFieldDef<IVedComponentProvider> ID = new M5StdFieldDefId<>() {
+
+    @Override
+    protected void doInit() {
+      super.doInit();
+      setFlags( 0 );
+    }
+  };
 
   /**
    * {@link IVedComponentProvider#nmName()}
    */
-  public static final IM5AttributeFieldDef<IVedComponentProvider> NAME = new M5StdFieldDefName<>();
+  public static final IM5AttributeFieldDef<IVedComponentProvider> NAME = new M5StdFieldDefName<>() {
+
+    @Override
+    protected Image doGetFieldValueIcon( IVedComponentProvider aEntity, EIconSize aIconSize ) {
+      IAtomicValue av = aEntity.params().getValue( TSID_ICON_ID, defaultValue() );
+      if( av.isAssigned() && av.atomicType() == EAtomicType.STRING ) {
+        String iconId = av.asString();
+        if( !iconId.isBlank() ) {
+          return iconManager().loadStdIcon( iconId, aIconSize );
+        }
+      }
+      return null;
+    }
+
+  };
 
   /**
    * {@link IVedComponentProvider#description()}
@@ -63,7 +83,7 @@ public class VedComponentProviderM5Model
    */
   public VedComponentProviderM5Model() {
     super( MODEL_ID, IVedComponentProvider.class );
-    addFieldDefs( ICON, ID, NAME, DESCRIPTION );
+    addFieldDefs( ID, NAME, DESCRIPTION );
   }
 
   @Override
