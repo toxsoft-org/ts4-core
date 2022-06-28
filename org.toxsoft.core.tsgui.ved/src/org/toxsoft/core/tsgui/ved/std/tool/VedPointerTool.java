bd680d7866b28ef63ffadcf37a6e824d6bbdca6f
vs <vs@LAPTOP-DSEKI2TA>
2022-06-28 13:00:28 +0300

    Промежуточный комит

--- org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/std/tool/VedPointerTool.java
+++ org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/std/tool/VedPointerTool.java
@@ -1,6 +1,6 @@
 package org.toxsoft.core.tsgui.ved.std.tool;
 
-import static org.toxsoft.core.tsgui.ved.impl.IVedResources.*;
+import static org.toxsoft.core.tsgui.ved.std.tool.ITsResources.*;
 
 import org.toxsoft.core.tsgui.bricks.ctx.*;
 import org.toxsoft.core.tsgui.ved.impl.*;
@@ -12,11 +12,11 @@ import org.toxsoft.core.tsgui.ved.impl.*;
  *
  * @author vs
  */
-public class PointerVedTool
+public class VedPointerTool
     extends AbstractVedTool {
 
-  protected PointerVedTool( ITsGuiContext aContext ) {
-    super( "pointerTool", STRN_TOOL_POINTER, STRD_TOOL_POINTER, "", aContext ); //$NON-NLS-1$
+  protected VedPointerTool( ITsGuiContext aContext ) {
+    super( "pointerTool", STR_N_TOOL_POINTER, STR_D_TOOL_POINTER, "", aContext ); //$NON-NLS-1$
     // TODO Auto-generated constructor stub
   }
 
