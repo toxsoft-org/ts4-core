b2dda5c5911045d68845c7af0facaa4043d476a4
goga <goga@GOGAL>
2022-08-06 13:22:53 +0300

    IVedEditorTool introduced

--- org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/extra/tools/IVedEditorTool.java
+++ org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/extra/tools/IVedEditorTool.java
@@ -23,7 +23,7 @@ public interface IVedEditorTool
 
   ITsUserInputListener screenInputListener();
 
-  ITsCollectionChangeListener componentsListListenetr();
+  ITsCollectionChangeListener componentsListListener();
 
   IVedScreen vedScreen();
 
