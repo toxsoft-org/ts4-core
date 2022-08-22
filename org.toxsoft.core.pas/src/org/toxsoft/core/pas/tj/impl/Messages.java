195e23b9c31dd9b6d18fdd98ce52cfe7b5a13864
tsdev <tsdev@mm-client-1>
2022-08-22 13:44:24 +0300

    Отладка первого запуска сервера s5 (uskat4)

--- org.toxsoft.core.pas/src/org/toxsoft/core/pas/tj/impl/Messages.java
+++ org.toxsoft.core.pas/src/org/toxsoft/core/pas/tj/impl/Messages.java
@@ -8,7 +8,7 @@ import java.util.ResourceBundle;
  */
 public class Messages {
 
-  private static final String BUNDLE_NAME = "ru.toxsoft.tslib.pas.tj.impl.messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = "org.toxsoft.core.pas.tj.impl.messages"; //$NON-NLS-1$
 
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle( BUNDLE_NAME );
 
