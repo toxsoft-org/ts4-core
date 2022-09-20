0d56b34c303320b343b583808348f8fc527b90a7
goga <goga@GOGAL>
2022-09-20 12:06:54 +0300

    IGwTimeFleetable introduced

---
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/gw/time/IGwTimeFleetable.java
@@ -0,0 +1,30 @@
+package org.toxsoft.core.tslib.gw.time;
+
+/**
+ * Mixin interface of Green World time-dependent entities.
+ * <p>
+ * The time of the Green World (TODO describe correctly):
+ * <ul>
+ * <li>time is measured in milliseconds from epoch start (UTC time starting from 01/01/1970);</li>
+ * <li>there is nothing like clock to ask time for, instead Green World is informed from outside when some amount of
+ * time has passed by method {@link #whenGwTimePassed(long)};</li>
+ * <li>there is no ability to calculate passed time between method calls. In fact, time may flow at any "speed" and in
+ * any direction - next call to {@link #whenGwTimePassed(long)} may have argument value less than previous call;</li>
+ * <li>thus the Green World time is flexible and may flow in any direction at any "speed".</li>
+ * </ul>
+ *
+ * @author hazard157
+ */
+public interface IGwTimeFleetable {
+
+  /**
+   * Informs entity that Green World time has passed.
+   * <p>
+   * Attention: entities should <b>not</b> get the current time themselves (in particular, they should <b>not</b> call
+   * {@link System#currentTimeMillis()} themselves), instead, they should use the argument as a timestamp.
+   *
+   * @param aGwTime long - Green World time in milliseconds from epoch start
+   */
+  void whenGwTimePassed( long aGwTime );
+
+}
