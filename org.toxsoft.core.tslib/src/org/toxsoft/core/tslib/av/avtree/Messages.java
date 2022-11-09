5fcd0c8d8c40580c90821a25a0e3d8c4eca456d8
hazard157 <goga@toxsoft.ru>
2022-11-09 14:26:37 +0400

    bug fix in org.toxsoft.core.tslib.av.avtree.Messages

--- org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/av/avtree/Messages.java
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/av/avtree/Messages.java
@@ -5,19 +5,20 @@ import java.util.*;
 @SuppressWarnings( "javadoc" )
 public class Messages {
 
-  private static final String BUNDLE_NAME = "ru.toxsoft.tslib.datavalue.avtree.messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = Messages.class.getName().toLowerCase();
 
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle( BUNDLE_NAME );
 
   private Messages() {
   }
 
-  public static String getString( String key ) {
+  public static String getString( String aKey ) {
     try {
-      return RESOURCE_BUNDLE.getString( key );
+      return RESOURCE_BUNDLE.getString( aKey );
     }
     catch( @SuppressWarnings( "unused" ) MissingResourceException e ) {
-      return '!' + key + '!';
+      return '!' + aKey + '!';
     }
   }
+
 }
