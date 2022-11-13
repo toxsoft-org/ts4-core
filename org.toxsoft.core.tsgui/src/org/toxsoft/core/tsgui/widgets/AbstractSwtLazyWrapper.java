485395e1858e174b0d04b8e32d166ae9e50b1232
goga <goga@GOGAL>
2022-11-13 12:19:31 +0300

    Added AbstractSwtLazyWrapper

---
+++ org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/widgets/AbstractSwtLazyWrapper.java
@@ -0,0 +1,56 @@
+package org.toxsoft.core.tsgui.widgets;
+
+import org.eclipse.swt.widgets.*;
+import org.toxsoft.core.tsgui.panels.lazy.*;
+import org.toxsoft.core.tslib.utils.errors.*;
+
+/**
+ * The wrapper over SWT {@link Control} to make it {@link ILazyControl}.
+ *
+ * @author hazard157
+ * @param <C> - type of the SWT control
+ */
+public abstract class AbstractSwtLazyWrapper<C extends Control>
+    implements ILazyControl<C> {
+
+  private C control = null;
+
+  /**
+   * Constructor.
+   */
+  public AbstractSwtLazyWrapper() {
+    // nop
+  }
+
+  // ------------------------------------------------------------------------------------
+  // ILazyControl
+  //
+
+  @Override
+  public C createControl( Composite aParent ) {
+    TsNullArgumentRtException.checkNull( aParent );
+    TsIllegalStateRtException.checkNoNull( control );
+    C c = doCreateControl( aParent );
+    TsInternalErrorRtException.checkNull( c );
+    control = c;
+    return c;
+  }
+
+  @Override
+  public C getControl() {
+    return control;
+  }
+
+  // ------------------------------------------------------------------------------------
+  // To implement
+  //
+
+  /**
+   * Subclass must create the SWT coontrol.
+   *
+   * @param aParent {@link Composite} - the parent composite
+   * @return &lt;C&gt; - create control, must not be <code>null</code>
+   */
+  protected abstract C doCreateControl( Composite aParent );
+
+}
