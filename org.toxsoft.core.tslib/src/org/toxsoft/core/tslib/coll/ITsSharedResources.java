2e8df7908599d4c8d9bf4767c878ad3ce5dd3086
hazard157 <goga@toxsoft.ru>
2022-11-16 16:02:29 +0400

    L10ნ fixes

--- org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/coll/ITsSharedResources.java
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/coll/ITsSharedResources.java
@@ -8,24 +8,10 @@ package org.toxsoft.core.tslib.coll;
 @SuppressWarnings( "javadoc" )
 public interface ITsSharedResources {
 
-  // FIXME L10N
-
-  String FMT_ERR_ITEM_NOT_FOUND_BY_KEY             = "%s: no item found by key '%s'";
-  String FMT_ERR_INV_INITIAL_CAPACITY              = "%s: invalid initial capacity %d";
-  String FMT_ERR_INV_CAPACITY                      = "%s: invalid capacity %d";
-  String FMT_ERR_INDEX_OUT_OF_RANGE                = "%s: индекс %d выходит за допустимые пределы 0 .. %d";
-  String FMT_ERR_REMOVING_ITEMS_COUNT_OUT_OF_RANGE =
-      "%s: количествово удаляемых элементов %d выходит за допустимые пределы 0 .. %d";
-
-  // ------------------------------------------------------------------------------------
-  // ru.RU
-  //
-
-  // String FMT_ERR_ITEM_NOT_FOUND_BY_KEY = "%s: элемент запрошенный по ключу '%s' не найден";
-  // String FMT_ERR_INV_INITIAL_CAPACITY = "%s: неверное значение %d начальной емкости";
-  // String FMT_ERR_INV_CAPACITY = "%s: неверное значение %d емкости";
-  // String FMT_ERR_INDEX_OUT_OF_RANGE = "%s: индекс %d выходит за допустимые пределы 0 .. %d";
-  // String FMT_ERR_REMOVING_ITEMS_COUNT_OUT_OF_RANGE =
-  // "%s: количествово удаляемых элементов %d выходит за допустимые пределы 0 .. %d";
+  String FMT_ERR_ITEM_NOT_FOUND_BY_KEY             = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
+  String FMT_ERR_INV_INITIAL_CAPACITY              = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
+  String FMT_ERR_INV_CAPACITY                      = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
+  String FMT_ERR_INDEX_OUT_OF_RANGE                = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
+  String FMT_ERR_REMOVING_ITEMS_COUNT_OUT_OF_RANGE = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
 
 }
