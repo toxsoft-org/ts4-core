33b8cb8f59f20c14db6d8ca1921b8d9233fb72c1
tsdev <tsdev@VJ-MCC-2>
2022-12-19 11:15:40 +0300

    PluginUtils l18n fix

--- org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/utils/plugins/impl/Messages.java
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/utils/plugins/impl/Messages.java
@@ -1,11 +1,12 @@
 package org.toxsoft.core.tslib.utils.plugins.impl;
 
-import java.util.*;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 @SuppressWarnings( { "javadoc", "unused" } )
 public class Messages {
 
-  private static final String BUNDLE_NAME = "ru.toxsoft.tslib.plugins.impl.messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = "org.toxsoft.core.tslib.utils.plugins.impl.messages"; //$NON-NLS-1$
 
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle( BUNDLE_NAME );
 
