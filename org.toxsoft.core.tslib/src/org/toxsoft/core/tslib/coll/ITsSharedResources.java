6bc234e5f558b834ec12c4d7a77d3912e9de6aea
goga <goga@GOGAH-PC-LIN>
2022-12-22 10:28:28 +0300

    l10n fixes

--- org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/coll/ITsSharedResources.java
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/coll/ITsSharedResources.java
@@ -8,10 +8,10 @@ package org.toxsoft.core.tslib.coll;
 @SuppressWarnings( "javadoc" )
 public interface ITsSharedResources {
 
-  String FMT_ERR_ITEM_NOT_FOUND_BY_KEY             = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
-  String FMT_ERR_INV_INITIAL_CAPACITY              = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
-  String FMT_ERR_INV_CAPACITY                      = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
-  String FMT_ERR_INDEX_OUT_OF_RANGE                = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
-  String FMT_ERR_REMOVING_ITEMS_COUNT_OUT_OF_RANGE = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
+  String FMT_ERR_ITEM_NOT_FOUND_BY_KEY             = Messages.getString( "FMT_ERR_ITEM_NOT_FOUND_BY_KEY" );             //$NON-NLS-1$
+  String FMT_ERR_INV_INITIAL_CAPACITY              = Messages.getString( "FMT_ERR_INV_INITIAL_CAPACITY" );              //$NON-NLS-1$
+  String FMT_ERR_INV_CAPACITY                      = Messages.getString( "FMT_ERR_INV_CAPACITY" );                      //$NON-NLS-1$
+  String FMT_ERR_INDEX_OUT_OF_RANGE                = Messages.getString( "FMT_ERR_INDEX_OUT_OF_RANGE" );                //$NON-NLS-1$
+  String FMT_ERR_REMOVING_ITEMS_COUNT_OUT_OF_RANGE = Messages.getString( "FMT_ERR_REMOVING_ITEMS_COUNT_OUT_OF_RANGE" ); //$NON-NLS-1$
 
 }
