e353b2232cfac895e0b3c8e7bac7b48202dc3f53
goga <goga@GOGAW-PC-LIIN>
2022-12-26 12:34:38 +0300

    l10n

--- org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/m5/gui/panels/impl/ITsResources.java
+++ org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/m5/gui/panels/impl/ITsResources.java
@@ -5,20 +5,19 @@ package org.toxsoft.core.tsgui.m5.gui.panels.impl;
  *
  * @author hazard157
  */
-@SuppressWarnings( "nls" )
 interface ITsResources {
 
   /**
    * {@link M5DefaultEntityFilterPanel}
    */
-  String TXT_M_TEXT = "Enter text to search";
-  String TXT_P_TEXT = "Filtering criteria text string";
+  String TXT_M_TEXT = Messages.getString( "TXT_M_TEXT" ); //$NON-NLS-1$
+  String TXT_P_TEXT = Messages.getString( "TXT_P_TEXT" ); //$NON-NLS-1$
 
   /**
    * {@link M5EntityPanelWithValeds}
    */
-  String FMT_ERR_NO_FACTORY_IN_PARAMS  = "Поле %s: в параметрах не задан класс фабрики редактора";
-  String FMT_CANT_SET_FIELD_VALUE      = "Поле %s: при присвоении занчения возникла ошибка %s";
-  String FMT_ERR_FIELD_VALIDATION_FAIL = "Поле %s: %s";
+  String FMT_ERR_NO_FACTORY_IN_PARAMS  = Messages.getString( "FMT_ERR_NO_FACTORY_IN_PARAMS" );  //$NON-NLS-1$
+  String FMT_CANT_SET_FIELD_VALUE      = Messages.getString( "FMT_CANT_SET_FIELD_VALUE" );      //$NON-NLS-1$
+  String FMT_ERR_FIELD_VALIDATION_FAIL = Messages.getString( "FMT_ERR_FIELD_VALIDATION_FAIL" ); //$NON-NLS-1$
 
 }
