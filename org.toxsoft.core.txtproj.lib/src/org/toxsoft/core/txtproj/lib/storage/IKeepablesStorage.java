cae996aac2d36c7ac13f3d7f338d15af4835dc9e
goga <goga@GOGAH-PC-LIN>
2023-03-24 20:46:06 +0300

    IKeepablesStorageRo extracted from IKeepablesStorage

--- org.toxsoft.core.txtproj.lib/src/org/toxsoft/core/txtproj/lib/storage/IKeepablesStorage.java
+++ org.toxsoft.core.txtproj.lib/src/org/toxsoft/core/txtproj/lib/storage/IKeepablesStorage.java
@@ -1,12 +1,9 @@
 package org.toxsoft.core.txtproj.lib.storage;
 
-import org.toxsoft.core.tslib.bricks.keeper.IEntityKeeper;
-import org.toxsoft.core.tslib.bricks.strio.StrioRtException;
-import org.toxsoft.core.tslib.coll.IList;
-import org.toxsoft.core.tslib.coll.basis.ITsClearable;
-import org.toxsoft.core.tslib.coll.basis.ITsCollection;
+import org.toxsoft.core.tslib.bricks.keeper.*;
+import org.toxsoft.core.tslib.coll.basis.*;
 import org.toxsoft.core.tslib.utils.errors.*;
-import org.toxsoft.core.txtproj.lib.ITsProject;
+import org.toxsoft.core.txtproj.lib.*;
 
 /**
  * Хранилище сериализуемых с помощью {@link IEntityKeeper} сущностей.
@@ -17,32 +14,7 @@ import org.toxsoft.core.txtproj.lib.ITsProject;
  * @author hazard157
  */
 public interface IKeepablesStorage
-    extends ITsClearable {
-
-  /**
-   * Определяет, если в хранилище раздел с указанным идентификатором.
-   *
-   * @param aId String - искомый идентификатор
-   * @return boolean - признак существования указанного раздела
-   */
-  boolean hasSection( String aId );
-
-  /**
-   * Загружает объект из разедла.
-   * <p>
-   * Если раздела с указанным идентификатором нет в хранилище, метод возвращает аргумент aDefault.
-   *
-   * @param <T> - конкретный тип объектов
-   * @param aId String - уникальный в рамках хранилища идентификатор (ИД-путь) раздела
-   * @param aKeeper {@link IEntityKeeper} - хранитель элементов
-   * @param aDefault &lt;T&gt - значение, возвращаемое при отсутствии раздела, может быть <code>null</code>
-   * @return &lt;T&gt - загруженный объект или aDefulat
-   * @throws TsNullArgumentRtException любой (кроме aDefault) аргумент = null
-   * @throws TsIllegalArgumentRtException идентификатор не ИД-путь
-   * @throws TsIoRtException ошибка чтения из хранилища
-   * @throws StrioRtException неверный формат хранения или в разеле хранятся другие сущности
-   */
-  <T> T readItem( String aId, IEntityKeeper<T> aKeeper, T aDefault );
+    extends IKeepablesStorageRo, ITsClearable {
 
   /**
    * Записывает объект в хранилище.
@@ -55,22 +27,6 @@ public interface IKeepablesStorage
    * @throws TsIoRtException ошибка записи в хранилище
    */
   <T> void writeItem( String aId, T aItem, IEntityKeeper<T> aKeeper );
-
-  /**
-   * Загружает коллекцию объектов.
-   * <p>
-   * Если раздела с указанным идентификатором нет в хранилище, метод возвращает пустой список.
-   *
-   * @param <T> - конкретный тип объектов
-   * @param aId String - уникальный в рамках хранилища идентификатор (ИД-путь) раздела
-   * @param aKeeper {@link IEntityKeeper} - хранитель элементов
-   * @return {@link IList}&lt;T&gt - загруженный список
-   * @throws TsNullArgumentRtException любой аргумент = null
-   * @throws TsIllegalArgumentRtException идентификатор не ИД-путь
-   * @throws TsIoRtException ошибка чтения из хранилища
-   * @throws StrioRtException неверный формат хранения или в разеле хранятся другие сущности
-   */
-  <T> IList<T> readColl( String aId, IEntityKeeper<T> aKeeper );
 
   /**
    * Записывает коллекцию объектов.
---
+++ org.toxsoft.core.txtproj.lib/src/org/toxsoft/core/txtproj/lib/storage/IKeepablesStorageRo.java
@@ -0,0 +1,56 @@
+package org.toxsoft.core.txtproj.lib.storage;
+
+import org.toxsoft.core.tslib.bricks.keeper.*;
+import org.toxsoft.core.tslib.bricks.strio.*;
+import org.toxsoft.core.tslib.coll.*;
+import org.toxsoft.core.tslib.utils.errors.*;
+
+/**
+ * Read-only methods of the {@link IKeepablesStorage} interface.
+ *
+ * @author hazard157
+ */
+public interface IKeepablesStorageRo {
+
+  /**
+   * Определяет, если в хранилище раздел с указанным идентификатором.
+   *
+   * @param aId String - искомый идентификатор
+   * @return boolean - признак существования указанного раздела
+   */
+  boolean hasSection( String aId );
+
+  /**
+   * Загружает объект из разедла.
+   * <p>
+   * Если раздела с указанным идентификатором нет в хранилище, метод возвращает аргумент aDefault.
+   *
+   * @param <T> - конкретный тип объектов
+   * @param aId String - уникальный в рамках хранилища идентификатор (ИД-путь) раздела
+   * @param aKeeper {@link IEntityKeeper} - хранитель элементов
+   * @param aDefault &lt;T&gt - значение, возвращаемое при отсутствии раздела, может быть <code>null</code>
+   * @return &lt;T&gt - загруженный объект или aDefulat
+   * @throws TsNullArgumentRtException любой (кроме aDefault) аргумент = null
+   * @throws TsIllegalArgumentRtException идентификатор не ИД-путь
+   * @throws TsIoRtException ошибка чтения из хранилища
+   * @throws StrioRtException неверный формат хранения или в разеле хранятся другие сущности
+   */
+  <T> T readItem( String aId, IEntityKeeper<T> aKeeper, T aDefault );
+
+  /**
+   * Загружает коллекцию объектов.
+   * <p>
+   * Если раздела с указанным идентификатором нет в хранилище, метод возвращает пустой список.
+   *
+   * @param <T> - конкретный тип объектов
+   * @param aId String - уникальный в рамках хранилища идентификатор (ИД-путь) раздела
+   * @param aKeeper {@link IEntityKeeper} - хранитель элементов
+   * @return {@link IList}&lt;T&gt - загруженный список
+   * @throws TsNullArgumentRtException любой аргумент = null
+   * @throws TsIllegalArgumentRtException идентификатор не ИД-путь
+   * @throws TsIoRtException ошибка чтения из хранилища
+   * @throws StrioRtException неверный формат хранения или в разеле хранятся другие сущности
+   */
+  <T> IList<T> readColl( String aId, IEntityKeeper<T> aKeeper );
+
+}
