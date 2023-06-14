aa9b7fa74f86939f6b055c5fec274ac126855df7
goga <goga@GOGAL-LIN>
2023-06-14 22:07:57 +0400

    L10N

--- org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/av/metainfo/ITsResources.java
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/av/metainfo/ITsResources.java
@@ -5,66 +5,37 @@ package org.toxsoft.core.tslib.av.metainfo;
  *
  * @author hazard157
  */
-@SuppressWarnings( "nls" )
 interface ITsResources {
 
-  // ------------------------------------------------------------------------------------
-  // en.EN
-  //
-
-  String STR_DEF_NAME                = "<<enter the name>>";
-  String STR_N_ID                    = "Identifier";
-  String STR_D_ID                    = "Unique string identifier (IDpath)";
-  String STR_N_NAME                  = "Name";
-  String STR_D_NAME                  = "Short human-readable name";
-  String STR_N_DESCRIPTION           = "Description";
-  String STR_D_DESCRIPTION           = "Descriptiove human-readable text";
-  String STR_N_DEFAULT_VALUE         = "Default";
-  String STR_D_DEFAULT_VALUE         = "Default value";
-  String STR_N_IS_MANDATORY          = "Mandatory?";
-  String STR_D_IS_MANDATORY          = "Mandatory option flag";
-  String STR_N_IDNAME                = "IDname";
-  String STR_D_IDNAME                = "String indetifier of format IDname";
-  String STR_N_IDPATH                = "IDpath";
-  String STR_D_IDPATH                = "String indetifier of format IDpath";
-  String STR_N_TS_BOOL               = "Логический";
-  String STR_D_TS_BOOL               = "Логический тип с отображением галочкой";
-  String STR_N_MAX_INCLUSIVE         = "MaxIncl";
-  String STR_D_MAX_INCLUSIVE         = "Maximal allowed value (inclusive)";
-  String STR_N_MIN_INCLUSIVE         = "MinIncl";
-  String STR_D_MIN_INCLUSIVE         = "Minimal allowed value (inclusive)";
-  String STR_N_MAX_EXCLUSIVE         = "MaxExcl";
-  String STR_D_MAX_EXCLUSIVE         = "Maximal allowed value (exclusive)";
-  String STR_N_MIN_EXCLUSIVE         = "MinExcl";
-  String STR_D_MIN_EXCLUSIVE         = "Minimal allowed value (exclusive)";
-  String STR_N_ICON_ID               = "Icon";
-  String STR_D_ICON_ID               = "ID of the icon";
-  String STR_N_LOCAL_DATE            = "Date";
-  String STR_D_LOCAL_DATE            = "Calendar date without zone info";
-  String STR_N_LOCAL_TIME            = "Time";
-  String STR_D_LOCAL_TIME            = "Time of daty without zone info";
-  String STR_N_LOCAL_DATE_TIME       = "Instant";
-  String STR_D_LOCAL_DATE_TIME       = "Moment in time without zone info";
-  String FMT_ERR_NO_MANDATORY_OPTION = "Mandatory option %s is not specified";
-
-  String FMT_ERR_NO_REGISTERED_DATA_DEF    = "Data defnition with ID '%s': not found";
-  String FMT_ERR_PARENT_DATA_DEF_AT_CHANGE = "Data defnition with ID '%s': can not change type from parent's %s to %s";
-  String FMT_ERR_DATA_DEF_AT_CHANGE        = "Data defnition with ID '%s': can not change type from %s to %s";
-  String FMT_ERR_DATA_DEF_IS_ALREADY       = "Data defnition with ID '%s': is already registered";
-
-  // ------------------------------------------------------------------------------------
-  // ru.RU
-  //
-
-  // String STR_DEF_NAME = "<<введите название>>";
-  // String STR_N_NAME = "Name";
-  // String STR_D_NAME = "Short human-readable name";
-  // String STR_N_DESCRIPTION = "Description";
-  // String STR_D_DESCRIPTION = "Descriptiove human-readable text";
-  // String STR_N_IDNAME = "ИД-имя";
-  // String STR_D_IDNAME = "Идентификатор в формате ИД-имени";
-  // String STR_N_IDPATH = "ИД-путь";
-  // String STR_D_IDPATH = "Идентификатор в формате ИД-пути";
-  // String FMT_ERR_NO_MANDATORY_OPTION = "Не задана объязательная опция %s";
+  String STR_DEF_NAME                      = Messages.getString( "STR_DEF_NAME" );                      //$NON-NLS-1$
+  String STR_ID                            = Messages.getString( "STR_ID" );                            //$NON-NLS-1$
+  String STR_ID_D                          = Messages.getString( "STR_ID_D" );                          //$NON-NLS-1$
+  String STR_NAME                          = Messages.getString( "STR_NAME" );                          //$NON-NLS-1$
+  String STR_NAME_D                        = Messages.getString( "STR_NAME_D" );                        //$NON-NLS-1$
+  String STR_DESCRIPTION                   = Messages.getString( "STR_DESCRIPTION" );                   //$NON-NLS-1$
+  String STR_DESCRIPTION_D                 = Messages.getString( "STR_DESCRIPTION_D" );                 //$NON-NLS-1$
+  String STR_DEFAULT_VALUE                 = Messages.getString( "STR_DEFAULT_VALUE" );                 //$NON-NLS-1$
+  String STR_DEFAULT_VALUE_D               = Messages.getString( "STR_DEFAULT_VALUE_D" );               //$NON-NLS-1$
+  String STR_IS_MANDATORY                  = Messages.getString( "STR_IS_MANDATORY" );                  //$NON-NLS-1$
+  String STR_IS_MANDATORY_D                = Messages.getString( "STR_IS_MANDATORY_D" );                //$NON-NLS-1$
+  String STR_IDNAME                        = Messages.getString( "STR_IDNAME" );                        //$NON-NLS-1$
+  String STR_IDNAME_D                      = Messages.getString( "STR_IDNAME_D" );                      //$NON-NLS-1$
+  String STR_IDPATH                        = Messages.getString( "STR_IDPATH" );                        //$NON-NLS-1$
+  String STR_IDPATH_D                      = Messages.getString( "STR_IDPATH_D" );                      //$NON-NLS-1$
+  String STR_TS_BOOL                       = Messages.getString( "STR_TS_BOOL" );                       //$NON-NLS-1$
+  String STR_TS_BOOL_D                     = Messages.getString( "STR_TS_BOOL_D" );                     //$NON-NLS-1$
+  String STR_ICON_ID                       = Messages.getString( "STR_ICON_ID" );                       //$NON-NLS-1$
+  String STR_ICON_ID_D                     = Messages.getString( "STR_ICON_ID_D" );                     //$NON-NLS-1$
+  String STR_LOCAL_DATE                    = Messages.getString( "STR_LOCAL_DATE" );                    //$NON-NLS-1$
+  String STR_LOCAL_DATE_D                  = Messages.getString( "STR_LOCAL_DATE_D" );                  //$NON-NLS-1$
+  String STR_LOCAL_TIME                    = Messages.getString( "STR_LOCAL_TIME" );                    //$NON-NLS-1$
+  String STR_LOCAL_TIME_D                  = Messages.getString( "STR_LOCAL_TIME_D" );                  //$NON-NLS-1$
+  String STR_LOCAL_DATE_TIME               = Messages.getString( "STR_LOCAL_DATE_TIME" );               //$NON-NLS-1$
+  String STR_LOCAL_DATE_TIME_D             = Messages.getString( "STR_LOCAL_DATE_TIME_D" );             //$NON-NLS-1$
+  String FMT_ERR_NO_MANDATORY_OPTION       = Messages.getString( "FMT_ERR_NO_MANDATORY_OPTION" );       //$NON-NLS-1$
+  String FMT_ERR_NO_REGISTERED_DATA_DEF    = Messages.getString( "FMT_ERR_NO_REGISTERED_DATA_DEF" );    //$NON-NLS-1$
+  String FMT_ERR_PARENT_DATA_DEF_AT_CHANGE = Messages.getString( "FMT_ERR_PARENT_DATA_DEF_AT_CHANGE" ); //$NON-NLS-1$
+  String FMT_ERR_DATA_DEF_AT_CHANGE        = Messages.getString( "FMT_ERR_DATA_DEF_AT_CHANGE" );        //$NON-NLS-1$
+  String FMT_ERR_DATA_DEF_IS_ALREADY       = Messages.getString( "FMT_ERR_DATA_DEF_IS_ALREADY" );       //$NON-NLS-1$
 
 }
