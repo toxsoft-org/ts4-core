c2a8dee164567c10525155d6781fd113b557718e
goga <goga@GOGAH-PC-LIN>
2023-09-23 12:03:13 +0300

    Working on VED

---
+++ org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/l10n_template/l10n/IHzResources.java
@@ -0,0 +1,12 @@
+package org.toxsoft.core.tsgui.ved.l10n_template.l10n;
+
+/**
+ * Localizable resources.
+ *
+ * @author hazard157
+ */
+interface IHzResources {
+
+  String STR_N_XXX = Messages.getString( "STR_N_XXX" ); //$NON-NLS-1$
+
+}
