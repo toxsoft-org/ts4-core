e617cfa0bbbf37a17f0d7903b948aa823bad0cc7
goga <goga@GOGAH-PC-LIN>
2023-10-01 17:17:20 +0300

    Working on VED

--- org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/devel/ITsGraphicsContext.java
+++ org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/devel/ITsGraphicsContext.java
@@ -6,10 +6,22 @@ import org.toxsoft.core.tsgui.graphics.lines.*;
 import org.toxsoft.core.tsgui.graphics.patterns.*;
 import org.toxsoft.core.tslib.bricks.geometry.*;
 
+/**
+ * TS graphics context wraps over {@link GC} and adds some TsGUI specific functionality.
+ * <p>
+ * The additional functions are:
+ * <ul>
+ * <li>access to the TsGUI (and E4 application) specific context via {@link #tsContext()};</li>
+ * <li>the concept of area to be drawn {@link #drawArea()}. When used in paint events drawing area is determined by
+ * event boundaries. In other cases (eg drawing on image or printer) drawing area corresponds to the physical
+ * dimensions. Anyway, there is no sense to draw outside drawing area;</li>
+ * <li>TsGUI specific settings and drawing methods like <code>setXxxInfo()</code> and <code>draw/fillXxx()</code>.</li>
+ * </ul>
+ *
+ * @author hazard157
+ */
 public interface ITsGraphicsContext
     extends ITsGuiContextable {
-
-  // PaintEvent event();
 
   GC gc();
 
