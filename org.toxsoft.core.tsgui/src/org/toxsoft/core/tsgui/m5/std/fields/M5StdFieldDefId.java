b700abaaf7ea6bfa1dadd4f3a579e32b6aadf5a3
goga <goga@GOGAL-LIN>
2023-10-10 20:09:04 +0300

    M5StdFieldDefXxx some constructor API changes

--- org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/m5/std/fields/M5StdFieldDefId.java
+++ org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/m5/std/fields/M5StdFieldDefId.java
@@ -5,16 +5,16 @@ import static org.toxsoft.core.tsgui.m5.std.fields.ITsResources.*;
 import static org.toxsoft.core.tslib.av.impl.AvUtils.*;
 import static org.toxsoft.core.tslib.av.metainfo.IAvMetaConstants.*;
 
-import org.toxsoft.core.tsgui.m5.model.impl.M5AttributeFieldDef;
-import org.toxsoft.core.tslib.av.IAtomicValue;
-import org.toxsoft.core.tslib.bricks.strid.IStridable;
-import org.toxsoft.core.tslib.utils.errors.TsNullArgumentRtException;
+import org.toxsoft.core.tsgui.m5.model.impl.*;
+import org.toxsoft.core.tslib.av.*;
+import org.toxsoft.core.tslib.bricks.strid.*;
+import org.toxsoft.core.tslib.utils.errors.*;
 
 /**
  * Attribute {@link IStridable#id()}.
  *
  * @author hazard157
- * @param <T> - modelled {@link IStridable} entity type
+ * @param <T> - modeled {@link IStridable} entity type
  */
 public class M5StdFieldDefId<T extends IStridable>
     extends M5AttributeFieldDef<T> {
@@ -23,20 +23,22 @@ public class M5StdFieldDefId<T extends IStridable>
    * Constructor.
    */
   public M5StdFieldDefId() {
-    this( STR_N_ID, STR_D_ID );
+    super( FID_ID, DDEF_IDPATH );
+    setNameAndDescription( STR_N_ID, STR_D_ID );
+    setFlags( M5FF_COLUMN | M5FF_INVARIANT );
   }
 
   /**
    * Constructor.
    *
-   * @param aName String - field name
-   * @param aDescription String - field description
+   * @param aIdsAndValues Object[] - identifier / value pairs for {@link #params()}
    * @throws TsNullArgumentRtException any argument = <code>null</code>
+   * @throws TsIllegalArgumentRtException number of elements in array is uneven
+   * @throws ClassCastException argument types convention is violated
    */
-  public M5StdFieldDefId( String aName, String aDescription ) {
-    super( FID_ID, DDEF_IDPATH );
-    setNameAndDescription( aName, aDescription );
-    setFlags( M5FF_COLUMN | M5FF_INVARIANT );
+  public M5StdFieldDefId( Object... aIdsAndValues ) {
+    super( FID_ID, DDEF_IDPATH, aIdsAndValues );
+    addFlags( M5FF_INVARIANT );
   }
 
   // ------------------------------------------------------------------------------------
