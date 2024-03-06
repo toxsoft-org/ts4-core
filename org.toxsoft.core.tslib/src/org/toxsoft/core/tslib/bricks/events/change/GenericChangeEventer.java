2f4b0415e7ea03b25e177aaec6ee73ebfccb4ac4
goga <goga@GOGAL-LIN>
2024-03-06 16:14:21 +0300

    Minor changes

--- org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/bricks/events/change/GenericChangeEventer.java
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/bricks/events/change/GenericChangeEventer.java
@@ -51,7 +51,7 @@ public class GenericChangeEventer
   }
 
   // ------------------------------------------------------------------------------------
-  // AbstractTsEventsFiringSupport
+  // AbstractTsEventsFiringSupport`
   //
 
   @Override
