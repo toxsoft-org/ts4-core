ba862921048ea13f78865851404051ec75bd8421
goga <goga@GOGAL>
2024-05-29 19:53:37 +0300

    ITsThreadExecutor moved to tslib

---
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/bricks/threadexec/TsRunnableLock.java
@@ -0,0 +1,37 @@
+package org.toxsoft.core.tslib.bricks.threadexec;
+
+import org.toxsoft.core.tslib.utils.logs.impl.LoggerUtils;
+
+/**
+ * Source: org.eclipse.swt.widgets.RunnableLock
+ *
+ * @author mvk
+ */
+class TsRunnableLock {
+
+  Runnable  runnable;
+  long      timestamp;
+  Thread    thread;
+  Throwable throwable;
+
+  TsRunnableLock( Runnable aRunnable, int aDelay ) {
+    runnable = aRunnable;
+    timestamp = System.currentTimeMillis() + aDelay;
+  }
+
+  boolean done() {
+    return runnable == null || throwable != null;
+  }
+
+  void run() {
+    if( runnable != null ) {
+      try {
+        runnable.run();
+      }
+      catch( RuntimeException | Error error ) {
+        LoggerUtils.defaultLogger().error( error );
+      }
+    }
+    runnable = null;
+  }
+}
