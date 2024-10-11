e8b5ca9b5fde61ba7fc085ede93ce5c5f4773d57
mvk <kovach.mike@gmail.com>
2024-10-11 12:59:29 +0700

    Принудительная maven-пересборка всех репозиториев 2

--- org.toxsoft.core.git.parser/src/org/toxsoft/core/git/parser/Main.java
+++ org.toxsoft.core.git.parser/src/org/toxsoft/core/git/parser/Main.java
@@ -21,6 +21,7 @@ public class Main {
    * diff --git a/ru.uskat.s5.server.histdata10/src/ru/uskat/s5/server/backend/supports/histdata/sequences/sync/IS5Resources.java b/ru.uskat.s5.server.histdata10/src/ru/uskat/s5/server/backend/supports/histdata/sequences/sync/IS5Resources.java
    * </code>
    * <p>
+   * 13:00
    */
   private static final String DIFF_AFTER_TOKEN = "diff --git a"; //$NON-NLS-1$
 
