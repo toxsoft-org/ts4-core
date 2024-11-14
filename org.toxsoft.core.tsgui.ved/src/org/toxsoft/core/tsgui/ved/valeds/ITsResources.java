16c531b1aabb1cce47da7b3ee0f14ebbd6be4663
vs <vs@LAPTOP-DSEKI2TA>
2024-11-14 14:22:34 +0300

    Localization

--- org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/valeds/ITsResources.java
+++ org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/valeds/ITsResources.java
@@ -14,4 +14,10 @@ interface ITsResources {
   String DLG_SELECT_VISEL_ID   = Messages.getString( "FMT_WARN_NO_VISEL_ID" );   //$NON-NLS-1$
   String DLG_SELECT_VISEL_ID_D = Messages.getString( "FMT_WARN_NO_VISEL_ID_D" ); //$NON-NLS-1$
 
+  /**
+   * {@link ValedMultiLineTextEditor}
+   */
+  String DLG_MULTILINE_INPUT_TITLE = "Ввод текста";
+  String DLG_MULTILINE_INPUT_TEXT  = "Текст: ";
+
 }
