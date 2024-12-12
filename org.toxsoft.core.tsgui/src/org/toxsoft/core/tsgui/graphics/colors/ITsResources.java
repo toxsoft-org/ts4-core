b39c5c094cea462f85624680f7cca719ac87006c
vs <vs@LAPTOP-DSEKI2TA>
2024-12-12 20:43:55 +0300

    Доработка ESwtSysColors

--- org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/graphics/colors/ITsResources.java
+++ org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/graphics/colors/ITsResources.java
@@ -46,28 +46,38 @@ interface ITsResources {
   /**
    * {@link ESwtSysColor}
    */
-  String STR_SYSCOL_WIDGET_DARK_SHADOW   = Messages.getString( "STR_SYSCOL_WIDGET_DARK_SHADOW" );   //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_DARK_SHADOW_D = Messages.getString( "STR_SYSCOL_WIDGET_DARK_SHADOW_D" ); //$NON-NLS-1$
-
+  String STR_SYSCOL_WIDGET_DARK_SHADOW           = Messages.getString( "STR_SYSCOL_WIDGET_DARK_SHADOW" );           //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_DARK_SHADOW_D         = Messages.getString( "STR_SYSCOL_WIDGET_DARK_SHADOW_D" );         //$NON-NLS-1$
   String STR_SYSCOL_WIDGET_DISABLED_FOREGROUND   = Messages.getString( "STR_SYSCOL_WIDGET_DISABLED_FOREGROUND" );   //$NON-NLS-1$
   String STR_SYSCOL_WIDGET_DISABLED_FOREGROUND_D = Messages.getString( "STR_SYSCOL_WIDGET_DISABLED_FOREGROUND_D" ); //$NON-NLS-1$
-
-  String STR_SYSCOL_TEXT_DISABLED_BACKGROUND   = Messages.getString( "STR_SYSCOL_TEXT_DISABLED_BACKGROUND" );   //$NON-NLS-1$
-  String STR_SYSCOL_TEXT_DISABLED_BACKGROUND_D = Messages.getString( "STR_SYSCOL_TEXT_DISABLED_BACKGROUND_D" ); //$NON-NLS-1$
-
-  String STR_SYSCOL_WIDGET_NORMAL_SHADOW      = Messages.getString( "STR_SYSCOL_WIDGET_NORMAL_SHADOW" );      //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_NORMAL_SHADOW_D    = Messages.getString( "STR_SYSCOL_WIDGET_NORMAL_SHADOW_D" );    //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_LIGHT_SHADOW       = Messages.getString( "STR_SYSCOL_WIDGET_LIGHT_SHADOW" );       //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_LIGHT_SHADOW_D     = Messages.getString( "STR_SYSCOL_WIDGET_LIGHT_SHADOW_D" );     //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW   = Messages.getString( "STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW" );   //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW_D = Messages.getString( "STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW_D" ); //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_BACKGROUND         = Messages.getString( "STR_SYSCOL_WIDGET_BACKGROUND" );         //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_BACKGROUND_D       = Messages.getString( "STR_SYSCOL_WIDGET_BACKGROUND_D" );       //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_BORDER             = Messages.getString( "STR_SYSCOL_WIDGET_BORDER" );             //$NON-NLS-1$
-  String STR_SYSCOL_WIDGET_BORDER_D           = Messages.getString( "STR_SYSCOL_WIDGET_BORDER_D" );           //$NON-NLS-1$
-
-  String STR_SYSCOL_LINK_FOREGROUND   = Messages.getString( "STR_SYSCOL_LINK_FOREGROUND" );   //$NON-NLS-1$
-  String STR_SYSCOL_LINK_FOREGROUND_D = Messages.getString( "STR_SYSCOL_LINK_FOREGROUND_D" ); //$NON-NLS-1$
+  String STR_SYSCOL_TEXT_DISABLED_BACKGROUND     = Messages.getString( "STR_SYSCOL_TEXT_DISABLED_BACKGROUND" );     //$NON-NLS-1$
+  String STR_SYSCOL_TEXT_DISABLED_BACKGROUND_D   = Messages.getString( "STR_SYSCOL_TEXT_DISABLED_BACKGROUND_D" );   //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_NORMAL_SHADOW         = Messages.getString( "STR_SYSCOL_WIDGET_NORMAL_SHADOW" );         //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_NORMAL_SHADOW_D       = Messages.getString( "STR_SYSCOL_WIDGET_NORMAL_SHADOW_D" );       //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_LIGHT_SHADOW          = Messages.getString( "STR_SYSCOL_WIDGET_LIGHT_SHADOW" );          //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_LIGHT_SHADOW_D        = Messages.getString( "STR_SYSCOL_WIDGET_LIGHT_SHADOW_D" );        //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW      = Messages.getString( "STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW" );      //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW_D    = Messages.getString( "STR_SYSCOL_WIDGET_HIGHLIGHT_SHADOW_D" );    //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_BACKGROUND            = Messages.getString( "STR_SYSCOL_WIDGET_BACKGROUND" );            //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_BACKGROUND_D          = Messages.getString( "STR_SYSCOL_WIDGET_BACKGROUND_D" );          //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_BORDER                = Messages.getString( "STR_SYSCOL_WIDGET_BORDER" );                //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_BORDER_D              = Messages.getString( "STR_SYSCOL_WIDGET_BORDER_D" );              //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_FOREGROUND            = Messages.getString( "STR_SYSCOL_WIDGET_FOREGROUND" );            //$NON-NLS-1$
+  String STR_SYSCOL_WIDGET_FOREGROUND_D          = Messages.getString( "STR_SYSCOL_WIDGET_FOREGROUND_D" );          //$NON-NLS-1$
+  String STR_SYSCOL_LIST_FOREGROUND              = Messages.getString( "STR_SYSCOL_LIST_FOREGROUND" );              //$NON-NLS-1$
+  String STR_SYSCOL_LIST_FOREGROUND_D            = Messages.getString( "STR_SYSCOL_LIST_FOREGROUND_D" );            //$NON-NLS-1$
+  String STR_SYSCOL_LIST_BACKGROUND              = Messages.getString( "STR_SYSCOL_LIST_BACKGROUND" );              //$NON-NLS-1$
+  String STR_SYSCOL_LIST_BACKGROUND_D            = Messages.getString( "STR_SYSCOL_LIST_BACKGROUND_D" );            //$NON-NLS-1$
+  String STR_SYSCOL_LIST_SELECTION               = Messages.getString( "STR_SYSCOL_LIST_SELECTION" );
+  String STR_SYSCOL_LIST_SELECTION_D             = Messages.getString( "STR_SYSCOL_LIST_SELECTION_D" );
+  String STR_SYSCOL_LIST_SELECTION_TEXT          = Messages.getString( "STR_SYSCOL_LIST_SELECTION_TEXT" );
+  String STR_SYSCOL_LIST_SELECTION_TEXT_D        = Messages.getString( "STR_SYSCOL_LIST_SELECTION_TEXT_D" );
+  String STR_SYSCOL_LINK_FOREGROUND              = Messages.getString( "STR_SYSCOL_LINK_FOREGROUND" );              //$NON-NLS-1$
+  String STR_SYSCOL_LINK_FOREGROUND_D            = Messages.getString( "STR_SYSCOL_LINK_FOREGROUND_D" );            //$NON-NLS-1$
+  String STR_SYSCOL_INFO_FOREGROUND              = Messages.getString( "STR_SYSCOL_INFO_FOREGROUND" );              //$NON-NLS-1$
+  String STR_SYSCOL_INFO_FOREGROUND_D            = Messages.getString( "STR_SYSCOL_INFO_FOREGROUND_D" );            //$NON-NLS-1$
+  String STR_SYSCOL_INFO_BACKGROUND              = Messages.getString( "STR_SYSCOL_INFO_BACKGROUND" );              //$NON-NLS-1$
+  String STR_SYSCOL_INFO_BACKGROUND_D            = Messages.getString( "STR_SYSCOL_INFO_BACKGROUND_D" );            //$NON-NLS-1$
 
   /**
    * {@link AbstractTsColorSourceKind}
