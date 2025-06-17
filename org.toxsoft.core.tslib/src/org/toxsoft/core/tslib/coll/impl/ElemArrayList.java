ebbb9e09cf1a25c4428e54ce9d68c797b5187f26
tsdev <tsdev@nm-dch>
2025-06-17 17:17:22 +0700

    tslib.core,ElemArrayList.src2list: fix (removed use HashSet)

--- org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/coll/impl/ElemArrayList.java
+++ org.toxsoft.core.tslib/src/org/toxsoft/core/tslib/coll/impl/ElemArrayList.java
@@ -183,18 +183,7 @@ public class ElemArrayList<E>
   protected ElemArrayList<E> src2list( ITsCollection<E> aSource ) {
     TsNullArgumentRtException.checkNull( aSource );
     Iterable<E> src = aSource;
-    int count = 0;
-    if( !allowDuplicates ) {
-      Set<E> set = new HashSet<>();
-      for( E e : aSource ) {
-        if( !hasElem( e ) ) {
-          set.add( e );
-          count++;
-        }
-      }
-      src = set;
-    }
-    ElemArrayList<E> result = new ElemArrayList<>( count, true );
+    ElemArrayList<E> result = new ElemArrayList<>( aSource.size(), allowDuplicates );
     for( E e : src ) {
       result.add( e );
     }
