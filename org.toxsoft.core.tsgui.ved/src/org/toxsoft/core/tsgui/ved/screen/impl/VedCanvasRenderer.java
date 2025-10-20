de9ce2397db26b5a54129883dd6c62f5a25e18bd
vs <vs@LAPTOP-DSEKI2TA>
2025-10-20 15:10:38 +0300

    CanvasRenderer

--- org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/screen/impl/VedCanvasRenderer.java
+++ org.toxsoft.core.tsgui.ved/src/org/toxsoft/core/tsgui/ved/screen/impl/VedCanvasRenderer.java
@@ -102,10 +102,12 @@ class VedCanvasRenderer
     float[] koeffs = new float[6];
     screenTransform.getElements( koeffs );
 
-    // aEvent.gc.setClipping( 0, 0, aEvent.width, aEvent.height );
-    int canvasW = (int)(canvasCfg.size().x() * canvasCfg.conversion().zoomFactor());
-    int canvasH = (int)(canvasCfg.size().x() * canvasCfg.conversion().zoomFactor());
-    aEvent.gc.setClipping( 0, 0, canvasW, canvasH );
+    String osName = System.getProperty( "os.name" );
+    if( osName.startsWith( "Windows" ) ) {
+      int canvasW = (int)(canvasCfg.size().x() * canvasCfg.conversion().zoomFactor());
+      int canvasH = (int)(canvasCfg.size().x() * canvasCfg.conversion().zoomFactor());
+      aEvent.gc.setClipping( 0, 0, canvasW, canvasH );
+    }
 
     aEvent.gc.setTransform( screenTransform );
 
