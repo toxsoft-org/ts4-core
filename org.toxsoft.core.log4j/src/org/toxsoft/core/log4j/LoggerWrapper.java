25f06239649cbf3d3763b98e5bfc9df8b741333d
goga <goga@GOGAL>
2025-10-27 22:57:55 +0300

    L10n

--- org.toxsoft.core.log4j/src/org/toxsoft/core/log4j/LoggerWrapper.java
+++ org.toxsoft.core.log4j/src/org/toxsoft/core/log4j/LoggerWrapper.java
@@ -1,78 +1,113 @@
 package org.toxsoft.core.log4j;
 
+import static org.toxsoft.core.log4j.ITsResources.*;
+
 import java.io.*;
 
 import org.apache.log4j.*;
 import org.apache.log4j.xml.*;
 import org.toxsoft.core.tslib.bricks.validator.*;
+import org.toxsoft.core.tslib.bricks.validator.vrl.*;
 import org.toxsoft.core.tslib.utils.*;
 import org.toxsoft.core.tslib.utils.errors.*;
 import org.toxsoft.core.tslib.utils.logs.*;
 import org.toxsoft.core.tslib.utils.logs.impl.*;
 
 /**
- * Оболочка над log4j-логером {@link Logger}, реализующий ТоксСофт-овский интерфейс {@link ILogger}.
+ * Wraps over Log4j {@link Logger} to implementa tslib interface {@link ILogger}.
  *
  * @author mvk
  */
 public class LoggerWrapper
     extends AbstractBasicLogger {
 
+  /**
+   * Log4j library expects in this system property the configuration XML file name.
+   * <p>
+   * Used in static method {@link #setScanPropertiesTimeout(long)}.
+   */
+  public static final String SYSPROP_LOG4J_CONFIG_FILE_NAME = "-Dlog4j.configuratio"; //$NON-NLS-1$
+
+  /**
+   * Valid extensions (without dot) and hence the formats of the Log4j configuration file.
+   * <p>
+   * Used in static method {@link #setScanPropertiesTimeout(long)}.
+   */
+  public static final String[] LOG4J_CONFIG_FILE_EXTS = { "properties", "xml" }; //$NON-NLS-1$//$NON-NLS-2$
+
   private final Logger source;
 
   /**
-   * Создает оболочку над указанным log4j-логером.
+   * Creates wrapper over the specified Log4j logger.
    *
-   * @param aSource {@link org.apache.log4j.Logger} - log4j-логер
-   * @return ILogger {@link ILogger} журнал
-   * @throws TsNullArgumentRtException аргумент = null
+   * @param aSource {@link org.apache.log4j.Logger} - Log4j-logger
+   * @return ILogger {@link ILogger} - created instance
+   * @throws TsNullArgumentRtException any argument = <code>null</code>
    */
   public static ILogger getLogger( org.apache.log4j.Logger aSource ) {
     return new LoggerWrapper( aSource );
   }
 
   /**
-   * Создает оболочку для указанной категории.
+   * Creates wrapper for the category specified by the name.
    *
-   * @param aName String - имя категории
-   * @return ILogger {@link ILogger} журнал
-   * @throws TsNullArgumentRtException аргумент = null
+   * @param aName String - the category name
+   * @return ILogger {@link ILogger} - created instance
+   * @throws TsNullArgumentRtException any argument = <code>null</code>
    */
   public static ILogger getLogger( String aName ) {
     return new LoggerWrapper( aName );
   }
 
   /**
-   * Создает оболочку для указанного класса.
+   * Creates wrapper to log messages from the specified class.
    *
-   * @param aClass Class - класс
-   * @return ILogger {@link ILogger} журнал
-   * @throws TsNullArgumentRtException аргумент = null
+   * @param aClass Class - the class
+   * @return ILogger {@link ILogger} - created instance
+   * @throws TsNullArgumentRtException any argument = <code>null</code>
    */
   public static ILogger getLogger( Class<?> aClass ) {
     return new LoggerWrapper( aClass );
   }
 
+  // GOGA 2025-10-27: as i found, this method is not used, anyway, it is deprecated!
+  // /**
+  // * Writes validation check results to the logger.
+  // *
+  // * @param aLogger {@link ILogger} - the logger
+  // * @param aResults {@link IValResList} - validation results
+  // * @throws TsNullArgumentRtException any argument = <code>null</code>
+  // */
+  // @Deprecated
+  // public static void resultsToLog( ILogger aLogger, IValResList aResults ) {
+  // TsNullArgumentRtException.checkNulls( aLogger, aResults );
+  // for( ValidationResult result : aResults.results() ) {
+  // resultToLog( aLogger, result );
+  // }
+  // }
+  // ---
+
   /**
-   * Запись в журнал результов
+   * Writes validation check results to the logger.
    *
-   * @param aLogger {@link ILogger} журнал
-   * @param aResults {@link IValResList} результат выполнения
-   * @throws TsNullArgumentRtException любой аргумент = null
+   * @param aLogger {@link ILogger} - the logger
+   * @param aResults {@link IVrList} - validation results
+   * @throws TsNullArgumentRtException any argument = <code>null</code>
    */
-  public static void resultsToLog( ILogger aLogger, IValResList aResults ) {
+  @Deprecated
+  public static void resultsToLog( ILogger aLogger, IVrList aResults ) {
     TsNullArgumentRtException.checkNulls( aLogger, aResults );
-    for( ValidationResult result : aResults.results() ) {
-      resultToLog( aLogger, result );
+    for( VrlItem item : aResults.items() ) {
+      resultToLog( aLogger, item.vr() );
     }
   }
 
   /**
-   * Запись в журнал результа.
+   * Writes single validation check result to the logger.
    *
-   * @param aLogger {@link ILogger} журнал
-   * @param aResult {@link ValidationResult} результат выполнения
-   * @throws TsNullArgumentRtException любой аргумент = null
+   * @param aLogger {@link ILogger} - the logger
+   * @param aResult {@link ValidationResult} - the result to be written
+   * @throws TsNullArgumentRtException any argument = <code>null</code>
    */
   public static void resultToLog( ILogger aLogger, ValidationResult aResult ) {
     TsNullArgumentRtException.checkNulls( aLogger, aResult );
@@ -83,7 +118,6 @@ public class LoggerWrapper
         }
         break;
       case WARNING:
-        // Переполнение очереди команд ожидающих выполнение. Старая команда удалена
         aLogger.warning( aResult.message() );
         break;
       case ERROR:
@@ -95,41 +129,25 @@ public class LoggerWrapper
   }
 
   // ------------------------------------------------------------------------------------
-  // Закрытые конструкторы
+  // Private constructors
   //
-  /**
-   * Создает оболочку над указанным log4j-логером.
-   *
-   * @param aSource {@link org.apache.log4j.Logger} - log4j-логер
-   * @throws TsNullArgumentRtException аргумент = null
-   */
+
   private LoggerWrapper( org.apache.log4j.Logger aSource ) {
     source = TsNullArgumentRtException.checkNull( aSource );
   }
 
-  /**
-   * Создает оболочку для указанной категории.
-   *
-   * @param aName String - имя категории
-   * @throws TsNullArgumentRtException аргумент = null
-   */
   private LoggerWrapper( String aName ) {
     source = org.apache.log4j.Logger.getLogger( TsNullArgumentRtException.checkNull( aName ) );
   }
 
-  /**
-   * Создает оболочку для указанного класса.
-   *
-   * @param aClass Class - класс
-   * @throws TsNullArgumentRtException аргумент = null
-   */
   private LoggerWrapper( Class<?> aClass ) {
     source = org.apache.log4j.Logger.getLogger( TsNullArgumentRtException.checkNull( aClass ) );
   }
 
   // ------------------------------------------------------------------------------------
-  // Переопределение методов базового класса AbstractBasicLogger
+  // AbstractBasicLogger
   //
+
   @Override
   public boolean isSeverityOn( ELogSeverity aSeverity ) {
     TsNullArgumentRtException.checkNull( aSeverity );
@@ -137,8 +155,9 @@ public class LoggerWrapper
   }
 
   // ------------------------------------------------------------------------------------
-  // Реализация недостающих методов интерфейса ILogger
+  // ILogger
   //
+
   @Override
   public void log( ELogSeverity aLogSeverity, String aMessage, Object... aArgs ) {
     Level level = ts2j( aLogSeverity );
@@ -160,52 +179,51 @@ public class LoggerWrapper
   }
 
   // ------------------------------------------------------------------------------------
-  // Внутренние методы
+  // implementation
   //
+
   private static Level ts2j( ELogSeverity aLogSeverity ) {
-    switch( aLogSeverity ) {
-      case DEBUG:
-        return Level.TRACE;
-      case ERROR:
-        return Level.ERROR;
-      case WARNING:
-        return Level.WARN;
-      case INFO:
-        return Level.INFO;
-      default:
-        throw new TsNotAllEnumsUsedRtException();
-    }
+    return switch( aLogSeverity ) {
+      case DEBUG -> Level.TRACE;
+      case ERROR -> Level.ERROR;
+      case WARNING -> Level.WARN;
+      case INFO -> Level.INFO;
+      default -> throw new TsNotAllEnumsUsedRtException();
+    };
 
   }
 
   /**
-   * Настроить период сканирования свойств журналирования
+   * Sets the logging properties configuration file rescan intervals.
+   * <p>
+   * Every <code>aIntervalMsecs</code> milliseconds application checks logger configuration file and applies changes.
+   * <p>
+   * Log4j library expects in this system property the configuration XML file name. This method requires
+   * {@link #SYSPROP_LOG4J_CONFIG_FILE_NAME} to contain valid file name with extension from
+   * {@link #LOG4J_CONFIG_FILE_EXTS} to be specified. Otherwise method write warning in {@link System#err} and does
+   * nothing.
    *
-   * @param aDelay long время (мсек) между проверками изменения свойств
+   * @param aIntervalMsecs long - rescan interval in milliseconds
    */
-  public static void setScanPropertiesTimeout( long aDelay ) {
+  public static void setScanPropertiesTimeout( long aIntervalMsecs ) {
     String log4jfilename = System.getProperty( "log4j.configuration" ); //$NON-NLS-1$
     if( log4jfilename == null ) {
-      System.err.println(
-          "LoggerWrapper.setScanPropertiesTimeout(): в системных свойствах (JVM) не установлен параметр : -Dlog4j.configuration" ); //$NON-NLS-1$
+      System.err.println( LOG_NO_LOG4J_CFG_FILE_IN_SYS_PROPS );
       return;
     }
     log4jfilename = log4jfilename.substring( 5 );
     if( new File( log4jfilename ).exists() ) {
       if( log4jfilename.endsWith( ".properties" ) ) { //$NON-NLS-1$
-        PropertyConfigurator.configureAndWatch( log4jfilename, aDelay );
+        PropertyConfigurator.configureAndWatch( log4jfilename, aIntervalMsecs );
         return;
       }
       if( log4jfilename.endsWith( ".xml" ) ) { //$NON-NLS-1$
-        DOMConfigurator.configureAndWatch( log4jfilename, aDelay );
+        DOMConfigurator.configureAndWatch( log4jfilename, aIntervalMsecs );
         return;
       }
-      System.err.println( "LoggerWrapper.setScanPropertiesTimeout(): недопустимое расширение файла для настроек log4j: " //$NON-NLS-1$
-          + log4jfilename );
+      System.err.println( LOG_NO_LOG4J_CFG_FILE_FOUND + log4jfilename );
     }
-    System.err.println(
-        "LoggerWrapper.setScanPropertiesTimeout(): не найден файл свойств указанный через параметр : -Dlog4j.configuration. filename = " //$NON-NLS-1$
-            + log4jfilename );
+    System.err.println( LOG_LOG4J_CFG_FILE_INV_EXT + log4jfilename );
   }
 
 }
