b4fe23d87728fdd5cc07b0c93a9f39afdd3e0db3
goga <goga@GOGA-MPC-LIN>
2026-01-10 13:48:05 +0300

    Minor changes

--- org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/panels/vecboard/IVecTabLayout.java
+++ org.toxsoft.core.tsgui/src/org/toxsoft/core/tsgui/panels/vecboard/IVecTabLayout.java
@@ -1,15 +1,12 @@
 package org.toxsoft.core.tsgui.panels.vecboard;
 
 /**
- * Раскладка, в которой каждый элемент является вклюадкой (Tab).
+ * Each element of this layout is a separate tab in the tab folder.
  * <p>
- * Порядок вкладок определяется порядком добавления элементов во вкладке.
+ * The order of the tabs is determined by the order in which items are added to the tab folder.
  * <p>
- * При создании раскладки, в конструкторе указываются параметры раслакдки:
- * <ul>
- * <li>{@link #isTabsAtBottom()} - true=показывать ярлыки вкладки внизу, false= вверху панели.</li>
- * </ul>
- * Каждый контроль в раскладку добавляется в виде отдельной вкладки.
+ * When adding component to the folder the layout data {@link IVecTabLayoutData} determines text, icon and tooltip of
+ * the tab.
  *
  * @author hazard157
  */
@@ -17,11 +14,11 @@ public interface IVecTabLayout
     extends IVecLayout<IVecTabLayoutData> {
 
   /**
-   * Возвращает признак показа ярлыков вкладок внизу панели.
+   * Returns the flag for showing tab labels at the bottom of the panel.
    *
-   * @return boolean - признак показа ярлыков вкладок внизу (а не вверху) панели<br>
-   *         <b>true</b> - ярлыки вкладок будут внизу панели;<br>
-   *         <b>false</b> - ярлыки будут сверху панели.
+   * @return boolean - the flag for showing tab labels at the bottom of the panel<br>
+   *         <b>true</b> - tab labels are at the bottom of the folder, below the components;<br>
+   *         <b>false</b> - tab labels are at the top of the folder, above the components.
    */
   boolean isTabsAtBottom();
 
